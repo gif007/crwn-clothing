4b98b22603c0ff2b926102586cd3ab8f692f5623
bard <carruthers.n.t@gmail.com>
2021-04-10 13:34:47 -0300

    added react-router-dom package and set up index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
-  return <HomePage />;
+  return (
+  <Switch>
+    <Route exact path="/" component={HomePage} />
+  </Switch>
+  )
 }
 
 export default App;
