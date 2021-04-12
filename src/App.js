69206671272b16f4cf18435aac45a8f9d70297ec
bard <carruthers.n.t@gmail.com>
2021-04-11 21:50:42 -0300

    added shop page, refactored collection preview with collection item, added more styling to collection previews

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-)
-
-const JacketsPage = () => (
-  <div>
-    <h1>Jackets</h1>
-  </div>
-)
-
-const ShoesPage = () => (
-  <div>
-    <h1>Shoes</h1>
-  </div>
-)
-
-const WomensPage = () => (
-  <div>
-    <h1>Womens Wear</h1>
-  </div>
-)
-
-const MensPage = () => (
-  <div>
-    <h1>Mens Wear</h1>
-  </div>
-)
 
 function App() {
   return (
   <Switch>
     <Route exact path="/" component={HomePage} />
-    <Route exact path="/shop/hats" component={HatsPage} />
-    <Route exact path="/shop/jackets" component={JacketsPage} />
-    <Route exact path="/shop/shoes" component={ShoesPage} />
-    <Route exact path="/shop/womens" component={WomensPage} />
-    <Route exact path="/shop/mens" component={MensPage} />
+    <Route path="/shop" component={ShopPage} />
   </Switch>
   )
 }
