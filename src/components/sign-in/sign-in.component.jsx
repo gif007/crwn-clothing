2c1e09cc5a566cdb449a1f0c7966f271271ce675
bard <carruthers.n.t@gmail.com>
2021-04-16 13:07:31 -0300

    implemented sign up with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,7 +52,13 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
+                        <CustomButton
+                            onClick={signInWithGoogle}
+                            type='button'
+                            isGoogleSignIn
+                        >
+                            Sign in with google
+                        </CustomButton>
                     </div>
                     
                 </form>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     measurementId: "G-EN3G0H83TM"
 };
 
+export const createUserProfileDocument = async (userAuth, otherData) => {
+    if (!userAuth) {return;}
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...otherData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message);
+        }
+    }
+
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
