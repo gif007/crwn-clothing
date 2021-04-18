84317c3a54d85ec5f3dc7fc270bda6ca95d40062
bard <carruthers.n.t@gmail.com>
2021-04-18 09:08:04 -0300

    updated application with reselect library. created selectors for getting cart, cart items and cart item count. refactored cart icon and cart dropdown to use selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 const CartIcon = ({ toggleCartHidden, totalCount }) => (
     <div className='cart-icon' onClick={ toggleCartHidden }>
@@ -15,8 +16,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-    totalCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    totalCount: selectCartItemsCount(state)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
